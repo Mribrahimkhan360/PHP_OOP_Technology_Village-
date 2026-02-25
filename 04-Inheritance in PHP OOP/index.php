cb655555acbc6e0819ebc279f8c5af9a5e6468b8
mribrahimkhan360 <mribrahimkhan360@gmail.com>
2026-02-25 13:33:52 +0600

    oop inheritence final keyword

--- 04-Inheritance in PHP OOP/index.php
+++ 04-Inheritance in PHP OOP/index.php
@@ -1,36 +1,50 @@
 <?php
 
-class man{
-    public $name;
-    public $age;
+// class man{
+//     public $name;
+//     public $age;
 
-    public function __construct($fname, $age){
-        $this->name = $fname;
-        $this->age = $age;
+//     public function __construct($fname, $age){
+//         $this->name = $fname;
+//         $this->age = $age;
+//     }
+
+//     protected function info(){
+//         return "<br />Name: " . $this->name . "<br />Age : " . $this->age;
+//     }
+
+// }
+
+// class info extends man{
+//     public $address;
+//     public function __construct($fname, $age, $address)
+//     {
+//         $this->name = $fname;
+//         $this->age = $age;
+//         $this->address = $address;
+//     }
+
+//     public function message()
+//     {
+//         return $this->name.' '. $this->age;
+//     }
+// }
+
+// $infos = new info("Ibrahim",25,"Shariatpur Dhaka Bangladesh");
+// // echo $infos->info();
+// // echo "<br />";
+// echo $infos->message();
+
+final class channel{
+    public function tv()
+    {
+        return "This is YT Channel.";
     }
-
-    protected function info(){
-        return "<br />Name: " . $this->name . "<br />Age : " . $this->age;
-    }
-
 }
 
-class info extends man{
-    public $address;
-    public function __construct($fname, $age, $address)
+class phpVideo extends channel{
+    public function video()
     {
-        $this->name = $fname;
-        $this->age = $age;
-        $this->address = $address;
-    }
-
-    public function message()
-    {
-        return $this->name.' '. $this->age;
+        return "This is a PHP Video.";
     }
 }
-
-$infos = new info("Ibrahim",25,"Shariatpur Dhaka Bangladesh");
-// echo $infos->info();
-// echo "<br />";
-echo $infos->message();
