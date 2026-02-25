c1534e5142f096efca2dcc040010a557325b071f
mribrahimkhan360 <mribrahimkhan360@gmail.com>
2026-02-25 11:48:30 +0600

    PHP OOP Constructor and Destructor in Bangla

--- 02-PHP OOP Constructor and Destructor in Bangla/index.php
+++ 02-PHP OOP Constructor and Destructor in Bangla/index.php
@@ -11,12 +11,20 @@ class man{
         $this->age = $age;
         $this->address = $address;
     }
+    function __destruct()
+    {
+        echo $this->name . '<br />' . $this->age . '<br />' . $this->address;
+    }
 
     
 }
 
+// __construct
+// $info = new man("Ibrahim",28,"Shariatpur");
+// echo "Informarion of man <br>";
+// echo "Name: ".$info->name."<br>";
+// echo "Age: ".$info->age."<br>";
+// echo "Address: ".$info->address."<br>";
+
+// __destruct
 $info = new man("Ibrahim",28,"Shariatpur");
-echo "Informarion of man <br>";
-echo "Name: ".$info->name."<br>";
-echo "Age: ".$info->age."<br>";
-echo "Address: ".$info->address."<br>";
