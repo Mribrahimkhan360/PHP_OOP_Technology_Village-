9c0dba2d0d1cb34848996995ffbf4471995e30ad
mribrahimkhan360 <mribrahimkhan360@gmail.com>
2026-02-25 11:40:07 +0600

    OOP construct function

--- 02-PHP OOP Constructor and Destructor in Bangla/index.php
+++ 02-PHP OOP Constructor and Destructor in Bangla/index.php
@@ -1,34 +1,22 @@
 <?php
 
-class man
-{
+class man{
     public $name;
     public $age;
     public $address;
 
-    function info($fname, $age , $address) {
-        // return "<br />Name: " . $this->name;
-        $this->name = $fname;
+    function __construct($name, $age, $address)
+    {
+        $this->name = $name;
         $this->age = $age;
         $this->address = $address;
-        return "<br />Name: " . $this->name . "<br />Age : " . $this->age . "<br />Address : " . $this->address;
     }
+
+    
 }
 
-// $man1 = new man();
-// $man2 = new man();
-
-// $man1->name='John';
-// $man2->name='Smith';
-
-// echo $man1->name;
-// echo $man1->info();
-// echo "<br />";
-
-// echo $man2->name;
-// echo $man2->info();
-
-// ==================
-
-$man1 = new man();
-echo $man1->info("Ibrahim", 30,"Shariatpur Dhaka Bangladesh");
+$info = new man("Ibrahim",28,"Shariatpur");
+echo "Informarion of man <br>";
+echo "Name: ".$info->name."<br>";
+echo "Age: ".$info->age."<br>";
+echo "Address: ".$info->address."<br>";
