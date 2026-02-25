dc85e9c059802015565ad4fd2ae6c682077ce2d9
mribrahimkhan360 <mribrahimkhan360@gmail.com>
2026-02-25 11:16:24 +0600

    function varibale pass

--- 01-What is class and object, properties and methods/index.php
+++ 01-What is class and object, properties and methods/index.php
@@ -6,20 +6,28 @@ class man
     public $age;
     public $address;
 
-    function info() {
-        return "<br />Name: " . $this->name;
+    function info($fname, $age) {
+        // return "<br />Name: " . $this->name;
+        $this->name = $fname;
+        $this->age = $age;
+        return "<br />Name: " . $this->name . "<br />Age : " . $this->age;
     }
 }
 
-$man1 = new man();
-$man2 = new man();
+// $man1 = new man();
+// $man2 = new man();
 
-$man1->name='John';
-$man2->name='Smith';
+// $man1->name='John';
+// $man2->name='Smith';
 
-echo $man1->name;
-echo $man1->info();
-echo "<br />";
+// echo $man1->name;
+// echo $man1->info();
+// echo "<br />";
 
-echo $man2->name;
+// echo $man2->name;
 // echo $man2->info();
+
+// ==================
+
+$man1 = new man();
+echo $man1->info("Ibrahim", 30);
